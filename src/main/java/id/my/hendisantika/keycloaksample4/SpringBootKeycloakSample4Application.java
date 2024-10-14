0bc86c488824fc4d8bca024dc572aaf7fc082204
hendisantika <hendisantika@yahoo.co.id>
2024-10-14 14:29:47 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/keycloaksample4/SpringBootKeycloakSample4Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.keycloaksample4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloakSample4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKeycloakSample4Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/keycloaksample4/SpringBootKeycloakSample4ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.keycloaksample4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakSample4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
