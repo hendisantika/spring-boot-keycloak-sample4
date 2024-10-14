6ebe5da77127deda920534c45ce853aaa3a88f18
hendisantika <hendisantika@yahoo.co.id>
2024-10-14 14:52:24 +0700

    Add getBearerToken🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/keycloaksample4/SpringBootKeycloakSample4ApplicationTests.java
+++ src/test/java/id/my/hendisantika/keycloaksample4/SpringBootKeycloakSample4ApplicationTests.java
@@ -3,16 +3,27 @@ package id.my.hendisantika.keycloaksample4;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import io.restassured.RestAssured;
 import jakarta.annotation.PostConstruct;
+import org.apache.http.client.utils.URIBuilder;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
 
 @Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -53,6 +64,33 @@ class SpringBootKeycloakSample4ApplicationTests {
 
             return "Bearer " + access_token;
         } catch (Exception e) {
+            LOGGER.error("Can't obtain an access token from Keycloak!", e);
+        }
+        return null;
+    }
+
+    protected String getBearerToken() {
+        try {
+            URI authorizationURI = new URIBuilder(keycloak.getAuthServerUrl()
+                    + "/realms/PowerRanger/protocol/openid-connect/token").build();
+            WebClient webclient = WebClient.builder().build();
+            MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+            formData.put("grant_type", Collections.singletonList("password"));
+            formData.put("client_id", Collections.singletonList("green_ranger"));
+            formData.put("username", Collections.singletonList("test-user"));
+            formData.put("password", Collections.singletonList("password"));
+
+            String result = webclient.post()
+                    .uri(authorizationURI)
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .body(BodyInserters.fromFormData(formData))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            JacksonJsonParser jsonParser = new JacksonJsonParser();
+            return "Bearer " + jsonParser.parseMap(result).get("access_token").toString();
+        } catch (URISyntaxException e) {
             LOGGER.error("Can't obtain an access token from Keycloak!", e);
         }
         return null;
