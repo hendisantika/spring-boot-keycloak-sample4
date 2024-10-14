a68edb01feb01fc07d4f948021164af1ae30e382
hendisantika <hendisantika@yahoo.co.id>
2024-10-14 14:36:25 +0700

    Add GrantedAuthority in JwtAuthConverter🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloaksample4/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/keycloaksample4/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.keycloaksample4.config;
 
+import id.my.hendisantika.keycloaksample4.JwtAuthConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
