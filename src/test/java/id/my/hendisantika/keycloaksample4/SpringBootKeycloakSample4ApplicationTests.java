0ff338951824a7085fbb437d6dbf2cb44acef4b8
hendisantika <hendisantika@yahoo.co.id>
2024-10-14 14:50:07 +0700

    Add SpringBootKeycloakSample4ApplicationTests🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/keycloaksample4/SpringBootKeycloakSample4ApplicationTests.java
+++ src/test/java/id/my/hendisantika/keycloaksample4/SpringBootKeycloakSample4ApplicationTests.java
@@ -1,13 +1,41 @@
 package id.my.hendisantika.keycloaksample4;
 
-import org.junit.jupiter.api.Test;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import io.restassured.RestAssured;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers(disabledWithoutDocker = true)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootKeycloakSample4ApplicationTests {
 
-    @Test
-    void contextLoads() {
+    private static final Logger LOGGER
+            = LoggerFactory.getLogger(SpringBootKeycloakSample4ApplicationTests.class.getName());
+
+    @Container
+    static KeycloakContainer keycloak
+            = new KeycloakContainer("quay.io/keycloak/keycloak:26.0")
+            .withRealmImportFile("keycloak/PowerRanger.json");
+
+    @LocalServerPort
+    private int port;
+
+    @DynamicPropertySource
+    static void registerResourceServerIssuerProperty(DynamicPropertyRegistry registry) {
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+                () -> keycloak.getAuthServerUrl() + "/realms/PowerRanger");
+    }
+
+    @PostConstruct
+    public void init() {
+        RestAssured.baseURI = "http://localhost:" + port;
     }
 
 }
